cc034b99b8641d5387f59244615abf87497fb90e
Alexa Hendry <alexahendry88@gmail.com>
2022-04-25 12:48:27 +0000

    Make piano keys play only for the duration of the click and make active with css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 let pianoKeys = document.querySelectorAll(".key");
 
 pianoKeys.forEach(function (pianoKey) {
-        pianoKey.addEventListener("click", function () {
+        pianoKey.addEventListener('click', function () {
                 return playNote(pianoKey);
             });
     });
 
 function playNote(pianoKey) {
-    let noteAudio = document.getElementById(pianoKey.dataset.note)
-    noteAudio.play()
+    let noteAudio = document.getElementById(pianoKey.dataset.note);
+    noteAudio.currentTime = 0;
+    noteAudio.play();
+    pianoKey.classList.add('active');
+    noteAudio.addEventListener('ended', function() {
+        pianoKey.classList.remove('active')
+    })
 };
