3e231af29547c1030dd9ac514b37d230367b1ffc
Alexa Hendry <alexahendry88@gmail.com>
2022-04-25 13:26:16 +0000

    Make piano playable with computer keyboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,32 @@
-let pianoKeys = document.querySelectorAll(".key");
+// virtual piano
+// define black and white keys on computer keyboard
+let WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm', 'a', 's', 'd', 'f', 'g', 'h', 'j'];
+let BLACK_KEYS = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
 
+// select all piano keys and attribute to variable
+let pianoKeys = document.querySelectorAll('.key');
+let whiteKeys = document.querySelectorAll('.key.white');
+let blackKeys = document.querySelectorAll('.key.black');
+
+// click event listener for piano keys
 pianoKeys.forEach(function (pianoKey) {
-        pianoKey.addEventListener('click', function () {
+        pianoKey.addEventListener('click', function() {
                 return playNote(pianoKey);
             });
     });
 
+// keydown event for piano keys
+document.addEventListener('keydown', function(event) {
+    if (event.repeat) return;
+    let pianoKey = event.key;
+    let whiteKeyIndex = WHITE_KEYS.indexOf(pianoKey);
+    let blackKeyIndex = BLACK_KEYS.indexOf(pianoKey);
+
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex]);
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex]);
+    })
+
+// play audio and define interaction with piano keys
 function playNote(pianoKey) {
     let noteAudio = document.getElementById(pianoKey.dataset.note);
     noteAudio.currentTime = 0;
