4c0b1c502f6301e77deec829c79702cb99d91f4e
Alexa Hendry <alexahendry88@gmail.com>
2022-05-02 16:39:16 +0000

    Add freestyle and challenge options on buttons

--- assets/js/challenge.js
+++ assets/js/challenge.js
@@ -1,3 +1,47 @@
+// virtual piano
+// This code was taken from YouTuber Web Dev Simplified and adapted - see details in the README
+
+// define black and white keys on computer keyboard
+let WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm', 'a', 's', 'd', 'f', 'g', 'h', 'j'];
+let BLACK_KEYS = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
+
+// select all piano keys and attribute to variable
+let pianoKeys = document.querySelectorAll('.key');
+let whiteKeys = document.querySelectorAll('.key.white');
+let blackKeys = document.querySelectorAll('.key.black');
+
+function freestyleOn() {
+    // click event listener for piano keys
+    pianoKeys.forEach(function (pianoKey) {
+        pianoKey.addEventListener('click', function () {
+            return playNote(pianoKey);
+        });
+    });
+
+    // keydown event for piano keys
+    document.addEventListener('keydown', function (event) {
+        if (event.repeat) return;
+        let pianoKey = event.key;
+        let whiteKeyIndex = WHITE_KEYS.indexOf(pianoKey);
+        let blackKeyIndex = BLACK_KEYS.indexOf(pianoKey);
+
+        if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex]);
+        if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex]);
+    });
+
+    // play audio and define interaction with piano keys
+    function playNote(pianoKey) {
+        let noteAudio = document.getElementById(pianoKey.dataset.note);
+        noteAudio.currentTime = 0;
+        noteAudio.play();
+        pianoKey.classList.add('active');
+        noteAudio.addEventListener('ended', function () {
+            pianoKey.classList.remove('active');
+        });
+    };
+}
+
+
 // virtual piano challenge game
 // this code was predominantly taken from the YouTube channel freeCodeCamp.org and adapted - see details in the README
 
@@ -66,22 +110,15 @@ let b2 = document.getElementById('key-b2');
 
 // event listener for clicking on challenge button
 challengeButton.addEventListener('click', function (event) {
-    if (challengeButton.checked == true) {
-        challengeOn = true;
-        playChallenge();
-    } else {
-        challengeOn = false;
-        clearKeyLights();
-        clearInterval(intervalId);
-    }
+    challengeOn = true;
+    playChallenge();
 });
 
 // event listener for clicking on freestyle button
-//freestyleButton.addEventListener('click', function (event) {
-    //challengeOn = false;//
-    //clearKeyLights();
-    //clearInterval(intervalId);
-//});//
+freestyleButton.addEventListener('click', function (event) {
+    challengeOn = false;
+    freestyleOn();
+});
 
 // play challenge function
 function playChallenge() {
